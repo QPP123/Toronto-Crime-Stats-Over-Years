2c6736175af478c40210dee2f9aaa5aa20d829ba
QPP123 <andrew.qiaokkuiyao@gmail.com>
2024-04-23 02:44:09 -0400

    download_data update

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -1,26 +1,37 @@
 #### Preamble ####
-# Purpose: Downloads and saves the data from [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Downloads and saves the data from City of Toronto Open Data
+# Author: Kuiyao Qiao
+# Date: 23 April 2024
+# Contact: kuiyao.qiao@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: None
 
 
 #### Workspace setup ####
 library(opendatatoronto)
-library(tidyverse)
-# [...UPDATE THIS...]
+library(dplyr)
 
-#### Download data ####
-# [...ADD CODE HERE TO DOWNLOAD...]
+# Create directory if it does not exist
+dir_path <- "data/raw_data/"
+if (!dir.exists(dir_path)) {
+  dir.create(dir_path, recursive = TRUE)
+}
 
+# get package
+package <- show_package("major-crime-indicators")
+package
 
+# get all resources for this package
+resources <- list_package_resources("major-crime-indicators")
+
+# identify datastore resources; by default, Toronto Open Data sets datastore resource format to CSV for non-geospatial and GeoJSON for geospatial resources
+datastore_resources <- filter(resources, tolower(format) %in% c('csv', 'geojson'))
+
+# load the first datastore resource as a sample
+data <- filter(datastore_resources, row_number()==1) %>% get_resource()
+data
 
 #### Save data ####
-# [...UPDATE THIS...]
 # change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
-
-         
+data_path <- paste0(dir_path, "major_crime_indicators.csv")
+write.csv(data, data_path, row.names = FALSE)
