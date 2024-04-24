eb39238bf49f9f4a5e8998ac5196f68842266cd9
QPP123 <andrew.qiaokkuiyao@gmail.com>
2024-04-24 03:22:43 -0400

    data clean update

--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -7,8 +7,13 @@
 # Pre-requisites: R 4.3.2, major-crime-indicators.csv
 
 #### Workspace setup ####
+# Install and load necessary libraries
+if (!requireNamespace("arrow", quietly = TRUE)) {
+  install.packages("arrow")
+}
 library(dplyr)
 library(readr)
+library(arrow)
 
 #### Clean data ####
 # Load data from the CSV file
@@ -17,7 +22,7 @@ data <- read_csv(file_path)
 
 # Remove unwanted columns
 data_cleaned <- select(data,
-                       -EVENT_UNIQUE_ID, -REPORT_DATE, -OCC_DATE, -REPORT_DOW, -OCC_DOW, -LOCATION_TYPE, -NEIGHBOURHOOD_158, -HOOD_140, -NEIGHBOURHOOD_140, -UCR_CODE, -UCR_EXT, -OCC_DOY, -REPORT_HOUR, -OCC_HOUR, -REPORT_DOY, -DIVISION, -HOOD_158)
+                       -EVENT_UNIQUE_ID, -REPORT_DATE, -OCC_DATE, -REPORT_DOW, -OCC_DOW, -NEIGHBOURHOOD_158, -HOOD_140, -NEIGHBOURHOOD_140, -UCR_CODE, -UCR_EXT, -OCC_DOY, -REPORT_HOUR, -OCC_HOUR, -REPORT_DOY, -DIVISION, -HOOD_158)
 
 # Ensure the directory exists
 dir_path <- "data/analysis_data/"
@@ -25,7 +30,8 @@ if (!dir.exists(dir_path)) {
   dir.create(dir_path, recursive = TRUE)
 }
 
-
 #### Save data ####
 data_path <- paste0(dir_path, "cleaned_major_crime_data.csv")
 write_csv(data_cleaned, data_path)
+data_path <- paste0(dir_path, "cleaned_major_crime_data.parquet")
+write_parquet(data_cleaned, data_path)
